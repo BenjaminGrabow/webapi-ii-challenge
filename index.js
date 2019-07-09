fadd6c6d40f3e1ea27a031bb856780a5334dbd79
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-09 15:16:16 +0200

    Feature: added update and delete

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const Hubs = require('./data/db');
+const Posts = require('./data/db');
 const server = express();
 
 server.use(express.json());
 
 server.get('/api/posts', async (req, res) => {
   try {
-    const hubs = await Hubs.find(req.query);
+    const hubs = await Posts.find(req.query);
     res.status(200).json(hubs);
   } catch (error) {
     console.log(error);
@@ -26,7 +26,6 @@ server.get('/api/posts/:id', async (req, res) => {
       res.status(404).json({ message: 'post not found' });
     }
   } catch (error) {
-    // log error to database
     console.log(error);
     res.status(500).json({
       errorMessage: 'Error retrieving the post',
@@ -37,9 +36,9 @@ server.get('/api/posts/:id', async (req, res) => {
 server.get('/api/posts/:id/comments', async (req, res) => {
     try {
       const { id } = req.params;
-      const hub = await Hubs.findById(id)
+      const hub = await Posts.findById(id)
       if (hub) {
-        const hubs = await Hubs.findPostComments(id)
+        const hubs = await Posts.findPostComments(id)
         res.status(200).json(hubs)
       } else {
         res.status(404).json({ message: 'Can\'t find that id!!' })
@@ -52,22 +51,35 @@ server.get('/api/posts/:id/comments', async (req, res) => {
   
   server.delete('/api/posts/:id', async (req, res) => {
     try {
-      const count = await Hubs.remove(req.params.id);
+      const count = await Posts.remove(req.params.id);
       if (count > 0) {
-        res.status(200).json({ message: 'The hub has been nuked' });
+        res.status(200).json({ message: 'The post has been deleted' });
       } else {
-        res.status(404).json({ message: 'The hub could not be found' });
+        res.status(404).json({ message: 'The post could not be found' });
       }
     } catch (error) {
-      // log error to database
       console.log(error);
       res.status(500).json({
-        message: 'Error removing the hub',
+        message: 'Error removing the post',
       });
     }
   });
 
-
+  server.put('/api/posts/:id', async (req, res) => {
+    try {
+      const hub = await Posts.update(req.params.id, req.body);
+      if (hub) {
+        res.status(200).json(hub);
+      } else {
+        res.status(404).json({ message: 'The post could not be found' });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        Errormessage: 'Error updating the post',
+      });
+    }
+  });
 
 server.listen(3500, () => {
   console.log('Server Running on http://localhost:3500 ***\n');
